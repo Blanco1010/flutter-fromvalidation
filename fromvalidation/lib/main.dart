88a11992f0167c9b6f67ea531edc3e61bdea8340
Juan Camilo Blanco Martinez <camilo3198@gmail.com>
2021-11-22 09:34:57 -0500

    add valitador for formulation

--- fromvalidation/lib/main.dart
+++ fromvalidation/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:fromvalidation/screen/home_screen.dart';
 import 'package:fromvalidation/screen/screens.dart';
--- fromvalidation/lib/screen/login_screen.dart
+++ fromvalidation/lib/screen/login_screen.dart
@@ -43,51 +43,68 @@ class _LoginForm extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       child: Form(
+          autovalidateMode: AutovalidateMode.onUserInteraction,
           child: Column(
-        children: [
-          TextFormField(
-            autocorrect: false,
-            cursorColor: Colors.red,
-            keyboardType: TextInputType.emailAddress,
-            decoration: InputDecorations.authInputDecoration(
-              hintText: 'Correo eletrónico',
-              labelText: 'example@gmail.com',
-              prefixIcon: Icon(Icons.email, color: Colors.red[400]),
-            ),
-          ),
-          SizedBox(height: 30),
-          TextFormField(
-            autocorrect: false,
-            obscureText: true,
-            cursorColor: Colors.red,
-            keyboardType: TextInputType.visiblePassword,
-            decoration: InputDecorations.authInputDecoration(
-              hintText: '**********',
-              labelText: 'Contraseña',
-              prefixIcon: Icon(Icons.password, color: Colors.red[400]),
-            ),
-          ),
-          SizedBox(height: 30),
-          MaterialButton(
-            padding: EdgeInsets.symmetric(horizontal: 100, vertical: 20),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
-            ),
-            color: Colors.red[500],
-            disabledColor: Colors.black,
-            elevation: 0,
-            child: Container(
-              child: Text(
-                'Ingresar',
-                style: TextStyle(color: Colors.white),
+            children: [
+              TextFormField(
+                autocorrect: false,
+                cursorColor: Colors.red,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecorations.authInputDecoration(
+                  hintText: 'Correo eletrónico',
+                  labelText: 'example@gmail.com',
+                  prefixIcon: Icon(Icons.email, color: Colors.red[400]),
+                ),
+                validator: (vaule) {
+                  String pattern =
+                      r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+                  RegExp regExp = new RegExp(pattern);
+
+                  if (vaule == '') return null;
+
+                  return regExp.hasMatch(vaule ?? '')
+                      ? null
+                      : 'El valor ingresado no luce como un correo';
+                },
               ),
-            ),
-            onPressed: () {
-              //TODO:
-            },
-          )
-        ],
-      )),
+              SizedBox(height: 30),
+              TextFormField(
+                  autocorrect: false,
+                  obscureText: true,
+                  cursorColor: Colors.red,
+                  keyboardType: TextInputType.visiblePassword,
+                  decoration: InputDecorations.authInputDecoration(
+                    hintText: '**********',
+                    labelText: 'Contraseña',
+                    prefixIcon: Icon(Icons.password, color: Colors.red[400]),
+                  ),
+                  validator: (vaule) {
+                    if (vaule == '') return null;
+
+                    if (vaule != null && vaule.length >= 6) return null;
+                    return 'La contraseña debe ser de 6 caracteres';
+                  }),
+              SizedBox(height: 30),
+              MaterialButton(
+                padding: EdgeInsets.symmetric(horizontal: 100, vertical: 20),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                color: Colors.red[500],
+                disabledColor: Colors.black,
+                elevation: 0,
+                child: Container(
+                  child: Text(
+                    'Ingresar',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                onPressed: () {
+                  //TODO:
+                },
+              )
+            ],
+          )),
     );
   }
 }
